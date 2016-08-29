ce12537505347cb017932060f59100e6f23afc5b
jonzielen <jzielenkievicz@nycgo.com>
2016-08-29 15:42:16 -0400

    add filter factory

--- app/src/todoApp.js
+++ app/src/todoApp.js
@@ -22,6 +22,18 @@ var model = {
 
 var todoApp = angular.module('todoApp', []);
 
+todoApp.filter('checkedItems', function() {
+  return function(items, showComplete) {
+    var results = [];
+    angular.forEach(items, function(item) {
+      if (item.status == false || showComplete == true) {
+        results.push(item)
+      }
+    });
+    return results;
+  }
+});
+
 todoApp.controller('ToDoCtrl', function ($scope) {
   $scope.todo = model;
 
@@ -36,12 +48,16 @@ todoApp.controller('ToDoCtrl', function ($scope) {
   $scope.incompleteCount = function() {
     var count = 0;
     angular.forEach($scope.todo.items, function(e) {
-      if (e.status == false) return count++;
+      if (!e.status) return count++;
     });
     return count;
   }
 
   $scope.warningLevel = function() {
     return ($scope.completeCount() <= 3) ? 'YES' : 'NO';
+  }
+
+  $scope.addNewItem = function(i) {
+    $scope.todo.items.push({item: i, status: false});
   }
 });
